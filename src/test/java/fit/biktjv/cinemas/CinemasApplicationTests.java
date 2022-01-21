141f344c0ec02bccc911c8486e926ec526ccbcd3
Kristina Sinkovskaya <sinkokri@fit.cvut.cz>
2022-01-21 20:24:05 +0100

    initial commit

---
+++ src/test/java/fit/biktjv/cinemas/CinemasApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.biktjv.cinemas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
