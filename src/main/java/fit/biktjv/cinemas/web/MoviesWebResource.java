8f486a725c73782dc5b4123be0eb9f5e71e7f794
Kristina Sinkovskaya <sinkokri@fit.cvut.cz>
2022-01-23 22:52:34 +0100

    added templates

--- src/main/java/fit/biktjv/cinemas/web/MoviesWebResource.java
+++ src/main/java/fit/biktjv/cinemas/web/MoviesWebResource.java
@@ -1,6 +1,7 @@
 package fit.biktjv.cinemas.web;
 
 
+import fit.biktjv.cinemas.integration.MovieDAO;
 import fit.biktjv.cinemas.integration.PlayDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,17 +9,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+
 @Controller
-@RequestMapping("/plays")
+@RequestMapping("/movies")
 public class PlaysWebResource {
 
     @Autowired
-    PlayDAO playDAO;
+    MovieDAO movieDAO;
 
 
     @GetMapping
     public String allPlays(Model model){
-        model.addAttribute("plays", playDAO.allMoviesPlayed());
+        model.addAttribute("movies", movieDAO.findAllMoviesPlayed());
+        model.addAttribute("localDate", LocalDate.now());
         return "playsTemplate";
     }
 
